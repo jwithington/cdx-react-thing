30517fff085834d4f7d3913dbbf6d61ad3b96757
Jim Withington <jamesrwithington@gmail.com>
2019-08-12 11:13:00 -0700

    checkpoint end of first day

---
+++ .next/static/webpack/static/development/pages/index.js.3eb267149f2db8b8f5d8.hot-update.js
@@ -0,0 +1,44 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Card/index.js":
+/*!**********************************!*\
+  !*** ./components/Card/index.js ***!
+  \**********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/extends */ "./node_modules/@babel/runtime-corejs2/helpers/esm/extends.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_objectWithoutProperties__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/objectWithoutProperties */ "./node_modules/@babel/runtime-corejs2/helpers/esm/objectWithoutProperties.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./styles.css */ "./components/Card/styles.css");
+/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_styles_css__WEBPACK_IMPORTED_MODULE_3__);
+
+
+var _jsxFileName = "/Users/jwithington/cdx-react-thing/components/Card/index.js";
+
+
+
+var Card = function Card(props) {
+  var children = props.children,
+      rest = Object(_babel_runtime_corejs2_helpers_esm_objectWithoutProperties__WEBPACK_IMPORTED_MODULE_1__["default"])(props, ["children"]);
+
+  return react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("div", Object(_babel_runtime_corejs2_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__["default"])({
+    className: _styles_css__WEBPACK_IMPORTED_MODULE_3___default.a.card
+  }, rest, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }), children);
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Card);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.3eb267149f2db8b8f5d8.hot-update.js.map
